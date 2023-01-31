955ce15b6b918397ecd4e8101bb24ba792c0b02f
polarzero <0xpolarzero@gmail.com>
2023-01-31 17:40:36 +0100

    Update background & separate components

--- src/components/canvas/Entity.jsx
+++ src/components/canvas/Entity.jsx
@@ -1,21 +1,42 @@
-import { useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
-import { useFrame } from '@react-three/fiber';
-import { useCursor } from '@react-three/drei';
+import { useFrame, useThree } from '@react-three/fiber';
+import { Html, useCursor } from '@react-three/drei';
 import * as THREE from 'three';
 
 import orbVertexShader from './shaders/orb/vertexShader';
 import orbFragmentShader from './shaders/orb/fragmentShader';
 import stores from '@/stores';
+import hooks from '@/hooks';
 
 const Entity = ({ route, ...props }) => {
   const router = useRouter();
+  const group = useRef(null);
+
+  // const [hovered, hover] = useState(false);
+  // useCursor(hovered);
+
+  const radius = 2;
+
+  useFrame(({ camera }) => {
+    // Make sure the entity is always at the center of the left side of the screen
+    group.current.position.x = -1.5 * (window.innerWidth / window.innerHeight);
+  });
+  return (
+    <>
+      <group ref={group} position={[-2, 0, 0]} {...props}>
+        <Background radius={radius} />
+        <Orb radius={radius} />
+      </group>
+    </>
+  );
+};
+
+const Orb = ({ radius }) => {
   const mesh = useRef(null);
-  const [hovered, hover] = useState(false);
   const { traits } = stores.useTraits();
 
   const count = 100;
-  const radius = 2;
 
   const particlesPosition = useMemo(() => {
     const positions = new Float32Array(count * 3);
@@ -50,7 +71,6 @@ const Entity = ({ route, ...props }) => {
     [],
   );
 
-  useCursor(hovered);
   useFrame((state, delta) => {
     // Time
     const t = state.clock.getElapsedTime();
@@ -63,19 +83,13 @@ const Entity = ({ route, ...props }) => {
     mesh.current.material.uniforms.uColorB.value = new THREE.Vector3(
       ...traits.color.vec3.colorB,
     );
-
-    // Make sure the orb is always at the center of the left side of the screen
-    mesh.current.position.x = -1.5 * (window.innerWidth / window.innerHeight);
   });
 
   return (
     <points
       ref={mesh}
-      position={[-2, 0, 0]}
-      {...props}
       // onClick={() => router.push(route)}
-      onPointerOver={() => hover(true)}
-      onPointerOut={() => hover(false)}>
+    >
       <bufferGeometry>
         <bufferAttribute
           attach='attributes-position'
@@ -93,6 +107,37 @@ const Entity = ({ route, ...props }) => {
       />
     </points>
   );
+};
+
+const Background = ({ radius }) => {
+  const { traits } = stores.useTraits();
+  const { updateTheme } = hooks.useTheme();
+  const color = `rgba(${traits.background.rgb}, 0.9)`;
+
+  useEffect(() => {
+    // document.querySelector('.container').style.background = color;
+    updateTheme(traits.background);
+  }, [traits.background.rgb]);
+
+  return null;
+  // <Html center prepend>
+  //   <div
+  //     style={{
+  //       position: 'absolute',
+  //       width: radius * 200,
+  //       height: radius * 200,
+  //       top: '50%',
+  //       left: '50%',
+  //       transform: 'translate(-50%, -50%)',
+  //       borderRadius: '50%',
+
+  //       // Fade out the border
+  //       // it shoud be filled on the inside, but fade out on the outside
+  //       // same but with a custom color
+  //       background: `radial-gradient(circle at 50% 50%, ${color} 0%, rgba(var(--background-main-rgb), 0) 100%)`,
+  //     }}
+  //   />
+  // </Html>
 };
 
 export default Entity;
