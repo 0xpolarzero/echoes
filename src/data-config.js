9cc018417f4f175b9b36e18732ba0c877f342afd
polarzero <0xpolarzero@gmail.com>
2023-01-31 12:46:21 +0100

    Add background and organize color

--- src/data-config.js
+++ src/data-config.js
@@ -6,33 +6,111 @@ export const traits = [
       {
         // Soothing Skies and Warm Sands
         name: 'Oceanic Blaze, Sunset Haze',
-        colorA: [0.34, 0.53, 0.96],
-        colorB: [0.97, 0.7, 0.45],
+        vec3: {
+          colorA: [0.34, 0.53, 0.96],
+          colorB: [0.97, 0.7, 0.45],
+        },
+        rgb: {
+          colorA: '87, 136, 245',
+          colorB: '248, 179, 115',
+        },
       },
       {
         name: 'Rose Horizon, Azure Skies',
-        colorA: [0.71, 0.49, 0.9],
-        colorB: [0.17, 0.51, 0.95],
+        vec3: {
+          colorA: [0.71, 0.49, 0.9],
+          colorB: [0.17, 0.51, 0.95],
+        },
+        rgb: {
+          colorA: '181, 125, 230',
+          colorB: '43, 130, 242',
+        },
       },
       {
         name: 'Mauve Twilight, Silver Fog',
-        colorA: [0.78, 0.38, 0.8],
-        colorB: [0.53, 0.53, 0.53],
+        vec3: {
+          colorA: [0.78, 0.38, 0.8],
+          colorB: [0.53, 0.53, 0.53],
+        },
+        rgb: {
+          colorA: '199, 97, 204',
+          colorB: '136, 136, 136',
+        },
       },
       {
         name: 'Amber Sunset, Jade Depths',
-        colorA: [0.97, 0.84, 0.37],
-        colorB: [0.1, 0.47, 0.45],
+        vec3: {
+          colorA: [0.97, 0.84, 0.37],
+          colorB: [0.1, 0.47, 0.45],
+        },
+        rgb: {
+          colorA: '247, 214, 94',
+          colorB: '25, 119, 114',
+        },
       },
       {
         name: 'Royal Amethyst, Misty Gray',
-        colorA: [0.69, 0.33, 0.96],
-        colorB: [0.84, 0.84, 0.84],
+        vec3: {
+          colorA: [0.69, 0.33, 0.96],
+          colorB: [0.84, 0.84, 0.84],
+        },
+        rgb: {
+          colorA: '176, 84, 245',
+          colorB: '215, 215, 215',
+        },
       },
       {
         name: 'Gold Sands, Sapphire Dreams',
-        colorA: [0.88, 0.65, 0.24],
-        colorB: [0.27, 0.56, 0.84],
+        vec3: {
+          colorA: [0.88, 0.65, 0.24],
+          colorB: [0.27, 0.56, 0.84],
+        },
+        rgb: {
+          colorA: '224, 166, 61',
+          colorB: '69, 143, 214',
+        },
+      },
+    ],
+  },
+  {
+    type: 'background',
+    label: 'Background',
+    values: [
+      {
+        name: 'Midnight Mystery',
+        color: [0.06, 0.06, 0.06],
+        hex: '#101010',
+        rgb: '16, 16, 16',
+      },
+      {
+        name: 'Golden Sunshine',
+        color: [0.97, 0.86, 0.43],
+        hex: '#F7DC6F',
+        rgb: '247, 220, 111',
+      },
+      {
+        name: 'Lush Meadow',
+        color: [0.84, 0.96, 0.89],
+        hex: '#D5F5E3',
+        rgb: '213, 245, 227',
+      },
+      {
+        name: 'Blushing Rose',
+        color: [0.94, 0.58, 0.54],
+        hex: '#F1948A',
+        rgb: '241, 148, 138',
+      },
+      {
+        name: 'Regal Lilac',
+        color: [0.73, 0.56, 0.8],
+        hex: '#BB8FCE',
+        rgb: '187, 143, 206',
+      },
+      {
+        name: 'Clear Skies',
+        color: [0.52, 0.76, 0.91],
+        hex: '#85C1E9',
+        rgb: '133, 193, 233',
       },
     ],
   },
