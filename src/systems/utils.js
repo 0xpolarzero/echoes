f18cbb1e66fe203c2b76e170506faa9aaeee2d0d
polarzero <0xpolarzero@gmail.com>
2023-01-31 12:46:50 +0100

    Add color maker for background

--- src/systems/utils.js
+++ src/systems/utils.js
@@ -1,15 +1,18 @@
-export const getGradient = ({ colorA, colorB }, hovered) => {
-  const colorAFormatted = `rgba(${colorA[0] * 100}, ${colorA[1] * 100}, ${
-    colorA[2] * 100
-  }, ${hovered ? 0.4 : 0.1})`;
-  const colorBFormatted = `rgba(${colorB[0] * 100}, ${colorB[1] * 100}, ${
-    colorB[2] * 100
-  }, ${hovered ? 0.4 : 0.1})`;
+export const getGradient = ({ colorA, colorB }, highlight) => {
+  const colorAFormatted = `rgba(${colorA}, ${highlight ? 0.4 : 0.1})`;
+  const colorBFormatted = `rgba(${colorB}, ${highlight ? 0.4 : 0.1})`;
 
   return {
     gradient: `linear-gradient(90deg, ${colorAFormatted} 0%, ${colorBFormatted} 100%)`,
-    shadow: `0 4px 30px rgba(${colorA[0] * 100}, ${colorA[1] * 100}, ${
-      colorA[2] * 100
-    }, ${hovered ? 0.4 : 0.1})`,
+    shadow: `0 4px 30px rgba(${colorA}, ${highlight ? 0.4 : 0.1})`,
+  };
+};
+
+export const getBackground = ({ rgb: color }, highlight) => {
+  const colorFormatted = `rgba(${color}, ${highlight ? 0.4 : 0.1})`;
+
+  return {
+    background: colorFormatted,
+    shadow: `0 4px 30px rgba(${color}, ${highlight ? 0.4 : 0.1})`,
   };
 };
