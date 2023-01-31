6c1af25ac63898ebdaa1448d41ac71f62c52d0aa
polarzero <0xpolarzero@gmail.com>
2023-01-31 17:40:51 +0100

    Reverse camera position

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload } from '@react-three/drei';
 export default function Scene({ children, ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas {...props} camera={{ position: [0, -4, 0], fov: 75 }}>
+    <Canvas {...props} camera={{ position: [0, 4, 0], fov: 75 }}>
       <directionalLight intensity={0.75} />
       <ambientLight intensity={0.75} />
       {children}
