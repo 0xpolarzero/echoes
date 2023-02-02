455b8c5debdf41f5bb75882ed1f3b2ce1a861d3d
polarzero <0xpolarzero@gmail.com>
2023-02-02 16:09:41 +0100

    Add loader

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,23 +7,32 @@ import '@/styles/index.css';
 const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: true });
 
 export default function App({ Component, pageProps = { title: 'orbs' } }) {
-  const ref = useRef();
+  const layout = useRef();
+  const loader = useRef();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && loader.current)
+      loader.current.classList.add('hidden');
+  }, []);
 
   return (
     <>
       <Header title={pageProps.title} />
 
-      <Layout ref={ref}>
+      <Layout ref={layout}>
         <Component {...pageProps} />
         {/* The canvas can either be in front of the dom or behind. If it is in front it can overlay contents.
          * Setting the event source to a shared parent allows both the dom and the canvas to receive events.
          * Since the event source is now shared, the canvas would block events, we prevent that with pointerEvents: none. */}
         {Component?.canvas && (
-          <Scene className='canvas' eventSource={ref} eventPrefix='client'>
+          <Scene className='canvas' eventSource={layout} eventPrefix='client'>
             {Component.canvas(pageProps)}
           </Scene>
         )}
       </Layout>
+      <div ref={loader} id='loader'>
+        <div className='loader' />
+      </div>
     </>
   );
 }
