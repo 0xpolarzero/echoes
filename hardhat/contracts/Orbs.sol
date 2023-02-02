5826f35ca907efc412e5adc25e0eb895886ec624
polarzero <0xpolarzero@gmail.com>
2023-02-02 23:50:55 +0100

    Add main functionalities

--- hardhat/contracts/Orbs.sol
+++ hardhat/contracts/Orbs.sol
@@ -22,13 +22,14 @@ contract Orbs is ERC721URIStorage {
 
     Counters.Counter private _tokenIds;
 
+    uint256 private immutable s_creationDate;
+    // TODO also mapping id to creation date
     uint256 private s_maxExpansion;
+
+    string[] private s_usedSignatures;
 
     // Allowed traits
     mapping(uint256 => string[]) private s_allowedTraits;
-
-    // ! check max expansion
-    // ! usedNames & check
 
     /**
      * @notice Constructor
@@ -54,7 +55,60 @@ contract Orbs is ERC721URIStorage {
 
         // Set max expansion (particles count)
         s_maxExpansion = _maxExpansion;
+
+        // Set creation date
+        s_creationDate = block.timestamp;
     }
+
+    // TODO public/external
+    function mint() public {
+        // TODO check if usedNames
+    }
+
+    // TODO enhance/expand
+    function enhance(_tokenId) public {
+        // TODO check if the token exists
+        // TODO check if the token is owned by the sender
+        // TODO check if the token has been enhanced since x
+        // TODO check max expansion
+    }
+
+    // TODO getTokenURI see name to override??
+    function getUri(uint256 _tokenId) public view returns (string memory) {
+        // TODO increment expand based on date
+    }
+
+    /**
+     * @dev Add new allowed traits
+     * @param _traitId The index in the mapping
+     * @param _traits An array of strings
+     */
+    function addTraits(
+        uint256 _traitId,
+        string[] memory _traits
+    ) external onlyOwner {
+        // If the trait already exists, add the new ones
+        if (s_allowedTraits[_traitId].length > 0) {
+            for (uint256 i = 0; i < _traits.length; i++) {
+                s_allowedTraits[_traitId].push(_traits[i]);
+            }
+        } else {
+            // If the trait does not exist, create it
+            s_allowedTraits[_traitId] = _traits;
+        }
+    }
+
+    /// Setters
+
+    /**
+     * @dev Set the max expansion
+     * @param _maxExpansion The new max expansion
+     */
+    function setMaxExpansion(uint256 _maxExpansion) external onlyOwner {
+        s_maxExpansion = _maxExpansion;
+    }
+
+    /// Verifiers
 
     /**
      * @dev Check the validity of a trait
@@ -82,22 +136,47 @@ contract Orbs is ERC721URIStorage {
     }
 
     /**
-     * @dev Add new allowed traits
-     * @param _traitId The index in the mapping
-     * @param _traits An array of strings
+     * @dev Check if a signature is available
+     * @param _signature The signature to check
      */
-    function addTraits(
-        uint256 _traitId,
-        string[] memory _traits
-    ) external onlyOwner {
-        // If the trait already exists, add the new ones
-        if (s_allowedTraits[_traitId].length > 0) {
-            for (uint256 i = 0; i < _traits.length; i++) {
-                s_allowedTraits[_traitId].push(_traits[i]);
+    function isSignatureAvailable(
+        string memory _signature
+    ) internal view returns (bool) {
+        string[] memory usedSignatures = s_usedSignatures;
+
+        for (uint256 i = 0; i < usedSignatures.length; i++) {
+            if (
+                keccak256(abi.encodePacked(usedSignatures[i])) ==
+                keccak256(abi.encodePacked(_signature))
+            ) {
+                return false;
             }
-        } else {
-            // If the trait does not exist, create it
-            s_allowedTraits[_traitId] = _traits;
         }
+    }
+
+    /// Getters
+
+    /**
+     * @dev Get the allowed traits
+     * @param _traitId The index in the mapping
+     */
+    function getTraits(
+        uint256 _traitId
+    ) external view returns (string[] memory) {
+        return s_allowedTraits[_traitId];
+    }
+
+    /**
+     * @dev Get the max expansion
+     */
+    function getMaxExpansion() external view returns (uint256) {
+        return s_maxExpansion;
+    }
+
+    /**
+     * @dev Get the used signatures
+     */
+    function getUsedSignatures() external view returns (string[] memory) {
+        return s_usedSignatures;
     }
 }
