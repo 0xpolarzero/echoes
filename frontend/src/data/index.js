6825ad471ac3d74eb11479524b2af0c46b922e71
polarzero <0xpolarzero@gmail.com>
2023-02-07 13:38:50 +0100

    Add comments to organize

--- frontend/src/data/index.js
+++ frontend/src/data/index.js
@@ -1,19 +1,26 @@
 import { ethers } from 'ethers';
+// Data
 import traits from '@/data/traits.js';
 import names from '@/data/names.json';
+// Constants
 import networkMapping from '@/data/constants/networkMapping.json';
 import abiMainnet from '@/data/constants/EchoesMainnet.json';
 import abiTestnet from '@/data/constants/EchoesTestnet.json';
 import queries from './constants/subgraphQueries';
+// Systems
+import calculateParticlesCount from './systems/calculateParticlesCount';
 
+// Metadata
 const description =
   'An enigma of light, figure, and atmosphere, a singular spectrum frozen in time.';
 const externalUrl = '';
 const background = '101010';
 
+// Constants
 const MINT_PRICE_ETH = 0.01;
 const MINT_PRICE_WEI = ethers.utils.parseEther(MINT_PRICE_ETH.toString());
 
+// Networks
 const defaultChainId = 5;
 const deployedChainIds = [1, 5, 80001, 421613];
 
@@ -27,6 +34,7 @@ const config = {
   abiMainnet,
   abiTestnet,
   subgraphQueries: queries,
+  calculateParticlesCount,
   MINT_PRICE_ETH,
   MINT_PRICE_WEI,
   defaultChainId,
