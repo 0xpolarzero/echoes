bfeca1949de9ae0c876e47b0eec8dbdbe887c935
polarzero <0xpolarzero@gmail.com>
2023-02-07 12:16:30 +0100

    Add useMint store

--- frontend/src/stores/index.js
+++ frontend/src/stores/index.js
@@ -1,7 +1,8 @@
 import useAudio from './useAudio';
 import useConfig from './useConfig';
+import useMint from './useMint';
 import useTraits from './useTraits';
 
-const stores = { useAudio, useConfig, useTraits };
+const stores = { useAudio, useConfig, useMint, useTraits };
 
 export default stores;
