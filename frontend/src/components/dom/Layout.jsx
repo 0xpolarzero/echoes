f715d2092322b1d4706e24f306ef1a56d41fdb6e
polarzero <0xpolarzero@gmail.com>
2023-02-10 18:19:36 +0100

    Update config on any change in useConfig

--- frontend/src/components/dom/Layout.jsx
+++ frontend/src/components/dom/Layout.jsx
@@ -11,7 +11,7 @@ import stores from '@/stores';
 
 const Layout = forwardRef(({ children, ...props }, ref) => {
   const localRef = useRef();
-  const Config = stores.useConfig((state) => state.Config);
+  const { Config } = stores.useConfig(/* (state) => state.Config */);
   const getEchoes = stores.useGraph((state) => state.getEchoes);
 
   useImperativeHandle(ref, () => localRef.current);
