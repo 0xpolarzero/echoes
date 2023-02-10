10c727f5db9765386d807ee59e8f89c4822aab09
polarzero <0xpolarzero@gmail.com>
2023-02-10 15:01:03 +0100

    Add close button (back)

--- frontend/src/components/dom/Explore/NavFilters.jsx
+++ frontend/src/components/dom/Explore/NavFilters.jsx
@@ -1,39 +1,49 @@
+import { useEffect, useState } from 'react';
+import { BsArrow90DegLeft } from 'react-icons/bs';
 import config from '@/data';
 import stores from '@/stores';
-import { useEffect, useState } from 'react';
 
 const NavFilters = () => {
-  const filterEchoesByChain = stores.useGraph(
-    (state) => state.filterEchoesByChain,
+  const { echoes, filterEchoesByChain, clickedEcho } = stores.useGraph(
+    (state) => ({
+      filterEchoesByChain: state.filterEchoesByChain,
+      echoes: state.echoes,
+      clickedEcho: state.clickedEcho,
+    }),
   );
   const [selected, setSelected] = useState(0);
 
   useEffect(() => {
     filterEchoesByChain(0);
-  }, [filterEchoesByChain]);
+  }, [echoes, filterEchoesByChain]);
 
   return (
     <div className='nav-filters'>
-      <button
-        onClick={() => {
-          filterEchoesByChain(0);
-          setSelected(0);
-        }}
-        className={selected === 0 ? 'selected' : ''}>
-        All echoes
+      <button className={clickedEcho ? 'close' : 'close hidden'}>
+        <BsArrow90DegLeft />
       </button>
-
-      {config.chains.map((chain) => (
+      <div className='filters'>
         <button
-          key={chain.id}
           onClick={() => {
-            filterEchoesByChain(chain.id);
-            setSelected(chain.id);
+            filterEchoesByChain(0);
+            setSelected(0);
           }}
-          className={selected === chain.id ? 'selected' : ''}>
-          {chain.name}
+          className={selected === 0 ? 'selected' : ''}>
+          All echoes
         </button>
-      ))}
+
+        {config.chains.map((chain) => (
+          <button
+            key={chain.id}
+            onClick={() => {
+              filterEchoesByChain(chain.id);
+              setSelected(chain.id);
+            }}
+            className={selected === chain.id ? 'selected' : ''}>
+            {chain.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
