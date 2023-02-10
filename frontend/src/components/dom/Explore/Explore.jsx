ffd6952f8a5dcf9386620a4f7baf5093db574ff2
polarzero <0xpolarzero@gmail.com>
2023-02-10 15:00:50 +0100

    Move Infos

--- frontend/src/components/dom/Explore/Explore.jsx
+++ frontend/src/components/dom/Explore/Explore.jsx
@@ -1,11 +1,11 @@
 import NavFilters from './NavFilters';
-import Infos from './Infos';
+import Infos from '../Infos';
 
 const Explore = () => {
   return (
     <>
       <NavFilters />
-      <Infos />
+      <Infos actions={false} />
     </>
   );
 };
